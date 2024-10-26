eb77492f989bab36c0d001fadc92e569d18ed1ef
waryduchess <santigard0808@gmail.com>
2024-10-26 14:21:54 -0500

    agregar bootstrap y carpetas
    inicio del proyecto

---
+++ pizzas.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="./css/">
+    <link href="./boots/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<a href="pizza1.html"><img src="./img/pizza1.jpeg"alt="Imagen de pizza de peperoni"></a><br>
+    
+</body>
+</html>
